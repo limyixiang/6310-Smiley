c241478ff969b94319ac96a310f3267e794f50ad
limyixiang <limyixiang2@gmail.com>
2024-05-29 01:30:26 +0800

    Minor changes to login page
    Changed the name requirements from 3 characters to just a required field for sign up page.
    
    Changed the error message for login page from "Email or Password does not exist." to "Incorrect Password." as this error message is more specific, and we already have an error message called "User not found." when the email exists in the database.

--- Orbital/backend/controllers/authController.js
+++ Orbital/backend/controllers/authController.js
@@ -1,105 +1,105 @@
 const User = require("../models/userModel");
-const {check, validationResult} = require("express-validator");
-var jwtToken = require('jsonwebtoken');
+const { check, validationResult } = require("express-validator");
+var jwtToken = require("jsonwebtoken");
 var { expressjwt: jwt } = require("express-jwt");
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 //SIGNUP
 exports.signup = async (req, res) => {
     const errors = validationResult(req);
 
-    if (!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         return res.status(422).json({
             error: errors.array()[0].msg,
-        })
+        });
     }
 
     const user = new User(req.body);
     user.save()
-        .then(user => {
+        .then((user) => {
             res.json({
                 id: user._id,
                 name: user.name,
                 email: user.email,
             });
         })
-        .catch(err => {
-            let errorMessage = 'Something went wrong.';
+        .catch((err) => {
+            let errorMessage = "Something went wrong.";
             if (err.code === 11000) {
-                errorMessage = 'User already exists, please sign in.'; 
+                errorMessage = "User already exists, please sign in.";
             }
             return res.status(500).json({ error: errorMessage });
         });
-}
-
+};
 
 //SIGNIN
 exports.signin = async (req, res) => {
-
     const errors = validationResult(req);
-    if (!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         return res.status(422).json({
             error: errors.array()[0].msg,
-        })
+        });
     }
 
-    const {email, password} = req.body;
-    await User.findOne({email: `${email}`})
-        .then(user => {
-            if (!user) {
-                return res.status(400).json({
-                    error: "User not found."
-                })
-            }
+    const { email, password } = req.body;
+    await User.findOne({ email: `${email}` }).then((user) => {
+        if (!user) {
+            return res.status(400).json({
+                error: "User not found.",
+            });
+        }
 
-            if (!user.authenticate(password)){
-                return res.status(401).json({
-                    error: "Email or Password does not exist."
-                })
-            }
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Incorrect Password.",
+            });
+        }
 
-            //Setting Cookies
-            const token = jwtToken.sign({ _id: user._id }, 'shhhhh');
-            res.cookie("token", token, {expire: new Date() + 9999})
-            const {_id, name, email} = user
-            return res.json({token, user: {_id, name, email}});
-        })
-
-}
+        //Setting Cookies
+        const token = jwtToken.sign({ _id: user._id }, "shhhhh");
+        res.cookie("token", token, { expire: new Date() + 9999 });
+        const { _id, name, email } = user;
+        return res.json({ token, user: { _id, name, email } });
+    });
+};
 
 //FORGET PASSWORD
 exports.forgetPassword = async (req, res) => {
     try {
         //Find user by email
         const user = await User.findOne({ email: req.body.email });
-        
+
         //If user is not found
         if (!user) {
-            return res.status(404).json({ error: "User with this email does not exist." });
+            return res
+                .status(404)
+                .json({ error: "User with this email does not exist." });
         }
 
         //Generate a unique JWT token for the user that contains the user's id
-        const token = jwtToken.sign({ _id: user._id }, 'shhhhh', { expiresIn: '10m' });
+        const token = jwtToken.sign({ _id: user._id }, "shhhhh", {
+            expiresIn: "10m",
+        });
 
         //Send the token to the user's email
         const transporter = nodemailer.createTransport({
-            service: 'gmail',
+            service: "gmail",
             auth: {
                 user: process.env.EMAIL,
-                pass: process.env.PASSWORD
-            }
+                pass: process.env.PASSWORD,
+            },
         });
 
         //Email configurations
         const mailOptions = {
             from: process.env.EMAIL,
             to: user.email,
-            subject: 'Reset Password Link',
+            subject: "Reset Password Link",
             html: `<h1>Reset Password</h1>
                 <h2>Please click on the link below to reset your password:</h2>
                 <a href='${process.env.CLIENT_URL}/resetpassword/${token}'>${process.env.CLIENT_URL}/resetpassword/${token}</a>
                 <p>This link is valid for 10 minutes.</p>
-            `
+            `,
         };
 
         //Send the email
@@ -107,7 +107,9 @@ exports.forgetPassword = async (req, res) => {
             if (error) {
                 return res.status(500).json({ error: error.message });
             } else {
-                return res.status(200).json({ message: 'Email has been sent to the user.' });
+                return res
+                    .status(200)
+                    .json({ message: "Email has been sent to the user." });
             }
         });
     } catch (error) {
@@ -121,20 +123,28 @@ exports.resetPassword = async (req, res) => {
         //Verify the token
         let decoded_token;
         try {
-            decoded_token = jwtToken.verify(req.params.token, 'shhhhh');
+            decoded_token = jwtToken.verify(req.params.token, "shhhhh");
         } catch (err) {
-            if (err.name === 'TokenExpiredError') {
-                return res.status(401).json({ error: 'Session expired. Generate a new link and try again.' });
+            if (err.name === "TokenExpiredError") {
+                return res
+                    .status(401)
+                    .json({
+                        error: "Session expired. Generate a new link and try again.",
+                    });
             } else {
                 // Token is invalid
-                return res.status(401).json({ error: 'Session is invalid. Check the link in your email and try again.' });
+                return res
+                    .status(401)
+                    .json({
+                        error: "Session is invalid. Check the link in your email and try again.",
+                    });
             }
         }
 
         //Find the user by the decoded id
         const user = await User.findById(decoded_token._id);
         if (!user) {
-            return res.status(401).json({ error: 'User not found.' });
+            return res.status(401).json({ error: "User not found." });
         }
 
         //Update the user's password
@@ -142,7 +152,9 @@ exports.resetPassword = async (req, res) => {
         await user.save();
 
         //Send success message
-        res.status(200).json({ message: 'Password has been reset successfully.' });
+        res.status(200).json({
+            message: "Password has been reset successfully.",
+        });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -152,23 +164,23 @@ exports.resetPassword = async (req, res) => {
 exports.signout = async (req, res) => {
     res.clearCookie("token");
     res.json({
-        message: "User has signed out."
+        message: "User has signed out.",
     });
-}
+};
 
-//Protected Route 
+//Protected Route
 exports.isSignedIn = jwt({
-    secret: 'shhhhh',
+    secret: "shhhhh",
     userProperty: "auth",
-    algorithms: ['HS256']
-})
+    algorithms: ["HS256"],
+});
 
 exports.isAuthenticated = (req, res, next) => {
     let checker = req.profile && req.auth && req.profile._id == req.auth._id;
     if (!checker) {
         return res.status(403).json({
-            error: "ACCESS DENIED"
+            error: "ACCESS DENIED",
         });
-    } 
+    }
     next();
-}
+};
--- Orbital/backend/routes/auth.js
+++ Orbital/backend/routes/auth.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const { check } = require('express-validator');
-const { signin, signup, signout, isSignedIn, forgetPassword, resetPassword } = require("../controllers/authController")
+const { check } = require("express-validator");
+const {
+    signin,
+    signup,
+    signout,
+    isSignedIn,
+    forgetPassword,
+    resetPassword,
+} = require("../controllers/authController");
 
 router.post(
     "/signup",
     [
-        check("name", "Name must be at least 3 characters long.").isLength({ min: 3 }),
+        check("name", "Name is required.").isLength({ min: 1 }),
         check("email", "Email is required.").isEmail(),
         // By default, a strong password is defined as having at least 8 characters, with at least 1 uppercase, 1 lowercase, 1 number and 1 symbol.
         // We are not enforcing the symbol requirement here.
-        check("password", "Password does not meet the requirements. Please try again.")
-            .isStrongPassword({ minSymbols: 0, })
+        check(
+            "password",
+            "Password does not meet the requirements. Please try again."
+        ).isStrongPassword({ minSymbols: 0 }),
     ],
     signup
 );
@@ -20,7 +29,7 @@ router.post(
     "/signin",
     [
         check("email", "Email is required.").isEmail(),
-        check("password", "Password is required.").isLength({ min: 1 })
+        check("password", "Password is required.").isLength({ min: 1 }),
     ],
     signin
 );
@@ -30,12 +39,11 @@ router.post("/forgetpassword", forgetPassword);
 // Password requirements are handled on the frontend
 router.post("/resetpassword/:token", resetPassword);
 
-router.get("/signout", signout)
+router.get("/signout", signout);
 
 //Protected Route for testing
 router.get("/testroute", isSignedIn, (req, res) => {
-    res.send("A protected route.")
+    res.send("A protected route.");
 });
-
 
 module.exports = router;
