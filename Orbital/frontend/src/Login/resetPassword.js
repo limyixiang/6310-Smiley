297e1efde33b560c01612e92317591f4feb7f475
limyixiang <e1121685@u.nus.edu>
2024-05-30 15:09:27 +0800

    Changed the checking of a strong password to be consistent with that of "Sign Up"
    Initially, we used the front-end to check for whether the provided password is a strong password, i.e. minimum of 8 characters, with 1 uppercase, 1 lowercase character, and 1 number.
    
    The "Sign Up" function checks the validity of the strong password using express.js, so I changed the checking of strong password of "Reset Password" to be consistent with that of the "Sign Up" function.
    
    Now, both functions uses express.js to check if the password provided is a strong password before proceeding to save the user in the database.

--- Orbital/frontend/src/Login/resetPassword.js
+++ Orbital/frontend/src/Login/resetPassword.js
@@ -1,53 +1,40 @@
-import React, { useState } from 'react';
-import { forgetPasswordReset } from '../Backend';
-import "./resetPassword.css"
+import React, { useState } from "react";
+import { forgetPasswordReset } from "../Backend";
+import "./resetPassword.css";
 
 // ResetPassword component for the forget password page
 export function ForgetPassword() {
-
     const [inputValue, setInputValue] = useState({
-        password: '',
-        password2: '',
-        error: '',
+        password: "",
+        password2: "",
+        error: "",
         loading: false,
-        success: false
+        success: false,
     });
 
     // Destructuring values from the state
     const { password, password2, error, loading, success } = inputValue;
 
-    // Checks for validity of password
-    const isPasswordValid = (password) => {
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-        return passwordRegex.test(password);
-    }
-
     // Handles changes in the input fields
-    const handleInputChange = name => event => {
+    const handleInputChange = (name) => (event) => {
         setInputValue({
             ...inputValue,
             error: false,
-            [name]: event.target.value
+            [name]: event.target.value,
         });
-    }
+    };
 
     // Submits the form data to the backend
-    const handleOnSubmit = async event => {
+    const handleOnSubmit = async (event) => {
         event.preventDefault();
 
         // Both passwords do not match
         if (password !== password2) {
-            setInputValue({ ...inputValue, error: "Passwords do not match.", success: false, loading: false });
-            return;
-        }
-
-        // Password is not valid
-        if (!isPasswordValid(password)) {
-            setInputValue({ 
-                ...inputValue, 
-                error: "Password should contain at least 8 characters, with at least 1 uppercase, 1 lowercase and 1 number.", 
-                success: false, 
-                loading: false 
+            setInputValue({
+                ...inputValue,
+                error: "Passwords do not match.",
+                success: false,
+                loading: false,
             });
             return;
         }
@@ -59,33 +46,52 @@ export function ForgetPassword() {
 
         // Placeholder for the forget password function calling the backend
         forgetPasswordReset({ password }, token)
-            .then(data => {
+            .then((data) => {
                 if (data.error) {
-                    setInputValue({ ...inputValue, error: data.error, success: false, loading: false });
+                    setInputValue({
+                        ...inputValue,
+                        error: data.error,
+                        success: false,
+                        loading: false,
+                    });
                 } else {
-                    setInputValue({ ...inputValue, password:'', password2:'', success: true });
+                    setInputValue({
+                        ...inputValue,
+                        password: "",
+                        password2: "",
+                        success: true,
+                    });
                 }
             })
             .catch();
-    }
+    };
 
     // Displays error message if there's any
     const errorMessage = () => {
         return (
-            <div className="error-message" style={{ display: error ? "" : "none", color: "red" }}>
+            <div
+                className="error-message"
+                style={{ display: error ? "" : "none", color: "red" }}
+            >
                 {error}
             </div>
         );
-    }
+    };
 
     // Displays success message upon successful reset of password
     const successMessage = () => {
         return (
-            <div className="success-message" style={{ display: success ? "" : "none", color: "green" }}>
-                <p>Password has been reset successfully. Please proceed to <a href="/signin">login</a>.</p>
+            <div
+                className="success-message"
+                style={{ display: success ? "" : "none", color: "green" }}
+            >
+                <p>
+                    Password has been reset successfully. Please proceed to{" "}
+                    <a href="/signin">login</a>.
+                </p>
             </div>
         );
-    }
+    };
 
     return (
         <div className="form-container">
@@ -94,27 +100,63 @@ export function ForgetPassword() {
                 {successMessage()}
                 <div className="form-group">
                     <label htmlFor="password">Enter your new password:</label>
-                    <input id="password" type="password" value={password} onChange={handleInputChange("password")} placeholder='Password' required />
+                    <input
+                        id="password"
+                        type="password"
+                        value={password}
+                        onChange={handleInputChange("password")}
+                        placeholder="Password"
+                        required
+                    />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="password">Re-enter your new password:</label>
-                    <input id="password2" type="password" value={password2} onChange={handleInputChange("password2")} placeholder='Re-enter password' required />
+                    <label htmlFor="password">
+                        Re-enter your new password:
+                    </label>
+                    <input
+                        id="password2"
+                        type="password"
+                        value={password2}
+                        onChange={handleInputChange("password2")}
+                        placeholder="Re-enter password"
+                        required
+                    />
                 </div>
-                <p className="password-requirements">Password should contain at least 8 characters, with at least 1 uppercase, 1 lowercase and 1 number.</p>
+                <p className="password-requirements">
+                    Password should contain at least 8 characters, with at least
+                    1 uppercase, 1 lowercase and 1 number.
+                </p>
                 {errorMessage()}
                 <div className="form-button">
-                    <button id='button' className ='button' onClick={handleOnSubmit} style={{ display: loading ? 'none' : 'block' }}>Reset Password</button>
-                    <div className="spinner" id='spinner' style={{ display: loading ? 'block' : 'none' }}>
+                    <button
+                        id="button"
+                        className="button"
+                        onClick={handleOnSubmit}
+                        style={{ display: loading ? "none" : "block" }}
+                    >
+                        Reset Password
+                    </button>
+                    <div
+                        className="spinner"
+                        id="spinner"
+                        style={{ display: loading ? "block" : "none" }}
+                    >
                         <div className="loading-spinner"></div>
                         <p>Loading...</p>
                     </div>
                 </div>
-                <div className='link'>
-                        <center><p className='resetpassword-to-login'><b><a href='/signin'>Back to Login</a></b></p></center>
+                <div className="link">
+                    <center>
+                        <p className="resetpassword-to-login">
+                            <b>
+                                <a href="/signin">Back to Login</a>
+                            </b>
+                        </p>
+                    </center>
                 </div>
             </div>
         </div>
     );
 }
- 
+
 export default ForgetPassword;
