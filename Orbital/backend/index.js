70637aca0318adef79ba4c347bb4851dcee5e68a
limyixiang <limyixiang2@gmail.com>
2024-05-31 15:19:05 +0800

    Update index.js

--- Orbital/backend/index.js
+++ Orbital/backend/index.js
@@ -4,13 +4,14 @@ const app = require("./app");
 //Database connection
 mongoose
     .connect(
-        "mongodb+srv://Smiley:6310-Smiley@6310-smiley.yxgidpp.mongodb.net/?retryWrites=true&w=majority&appName=6310-Smiley"
+        process.env.MONGODB_URI ||
+            "mongodb+srv://Smiley:6310-Smiley@6310-smiley.yxgidpp.mongodb.net/?retryWrites=true&w=majority&appName=6310-Smiley"
     )
     .then(() => console.log("Database connected successfully"))
     .catch((err) => console.log("Database connection failed", err));
 
 //Starting the application
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`app is running at ${port}`);
