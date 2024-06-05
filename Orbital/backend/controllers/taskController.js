327f81442c8916130c076b82a49dcf6a67383b79
limyixiang <limyixiang2@gmail.com>
2024-06-06 03:20:45 +0800

    edited course model
    course model now has 2 tasks array, sorted by deadline and priority level respectively, instead of just 1 task array.

--- Orbital/backend/controllers/taskController.js
+++ Orbital/backend/controllers/taskController.js
@@ -3,6 +3,83 @@ const User = require("../models/userModel");
 const Course = require("../models/courseModel");
 const { validationResult } = require("express-validator");
 
+const sortByDate = async (arr, newDeadline, task) => {
+    console.log(arr);
+    const numTasks = arr.length;
+    for (let i = 0; i < numTasks; i++) {
+        const iTask = await Task.findById(arr[i]);
+        const iDeadline = new Date(iTask.dueDate).getTime();
+        if (newDeadline < iDeadline) {
+            arr.splice(i, 0, task);
+            break;
+        } else if (newDeadline == iDeadline) {
+            if (i === numTasks - 1) {
+                arr.splice(i + 1, 0, task);
+                break;
+            } else {
+                const nextDeadline = new Date(
+                    (await Task.findById(arr[i + 1])).dueDate
+                ).getTime();
+                if (newDeadline < nextDeadline) {
+                    arr.splice(i + 1, 0, task);
+                    break;
+                }
+            }
+        } else if (i === numTasks - 1) {
+            arr[numTasks] = task;
+        }
+    }
+    return arr;
+};
+
+const sortByPriority = async (arr, newDeadline, task) => {
+    const numTasks = arr.length;
+    for (let i = 0; i < numTasks; i++) {
+        const iTask = await Task.findById(arr[i]);
+        if (task.priority === "High") {
+            if (iTask.priority !== "High") {
+                arr.splice(i, 0, task);
+                break;
+            } else {
+                const iDeadline = new Date(iTask.dueDate).getTime();
+                if (newDeadline < iDeadline) {
+                    arr.splice(i, 0, task);
+                    break;
+                } else if (newDeadline === iDeadline) {
+                    if (i === numTasks - 1) {
+                        arr.splice(i + 1, 0, task);
+                        break;
+                    } else {
+                        const nextDeadline = new Date(
+                            (await Task.findById(arr[i + 1])).dueDate
+                        ).getTime();
+                        if (newDeadline < nextDeadline) {
+                            arr.splice(i + 1, 0, task);
+                            break;
+                        }
+                    }
+                }
+            }
+        } else {
+            if (iTask.priority === "Low") {
+                const iDeadline = new Date(iTask.dueDate).getTime();
+                if (newDeadline < iDeadline) {
+                    arr.splice(i, 0, task);
+                    break;
+                } else if (newDeadline === iDeadline) {
+                    arr.splice(i + 1, 0, task);
+                    break;
+                }
+            }
+        }
+        if (i === numTasks - 1) {
+            console.log("here");
+            arr[numTasks] = task;
+        }
+    }
+    return arr;
+};
+
 exports.createTask = async (req, res) => {
     try {
         const errors = validationResult(req);
@@ -22,9 +99,9 @@ exports.createTask = async (req, res) => {
             user: user,
             course: course,
         });
-        const userTasksByDate = user.tasksByDate;
-        const userTasksByPriority = user.tasksByPriority;
-        const courseTasks = course.tasks;
+        var userTasksByDate = user.tasksByDate;
+        var userTasksByPriority = user.tasksByPriority;
+        const courseTasksByDate = course.tasksByDate;
 
         // Insert new Task into user's array of tasks
         // Note that this only takes into acount the dueDate of the task and not the priority of the course yet
@@ -36,83 +113,99 @@ exports.createTask = async (req, res) => {
             const numTasks = userTasksByDate.length;
             const newDeadline = new Date(task.dueDate).getTime();
             // Insert new Task into user's array of tasks that is sorted by date
-            for (let i = 0; i < numTasks; i++) {
-                const iTask = await Task.findById(userTasksByDate[i]);
-                const iDeadline = new Date(iTask.dueDate).getTime();
-                if (newDeadline < iDeadline) {
-                    userTasksByDate.splice(i, 0, task);
-                    break;
-                } else if (newDeadline == iDeadline) {
-                    if (i === numTasks - 1) {
-                        userTasksByDate.splice(i + 1, 0, task);
-                        break;
-                    } else {
-                        const nextDeadline = new Date(
-                            (
-                                await Task.findById(userTasksByDate[i + 1])
-                            ).dueDate
-                        ).getTime();
-                        if (newDeadline < nextDeadline) {
-                            userTasksByDate.splice(i + 1, 0, task);
-                            break;
-                        }
-                    }
-                } else if (i === numTasks - 1) {
-                    userTasksByDate[numTasks] = task;
-                }
-            }
+            sortByDate(userTasksByDate, newDeadline, task)
+                .then((arr) => {
+                    userTasksByDate = arr;
+                })
+                .then(() => {
+                    sortByPriority(userTasksByPriority, newDeadline, task)
+                        .then((arr) => {
+                            userTasksByPriority = arr;
+                        })
+                        .then(() => {
+                            user.save();
+                            console.log("sorted by priority");
+                        });
+                });
+            // .then(() => user.save());
+            // for (let i = 0; i < numTasks; i++) {
+            //     const iTask = await Task.findById(userTasksByDate[i]);
+            //     const iDeadline = new Date(iTask.dueDate).getTime();
+            //     if (newDeadline < iDeadline) {
+            //         userTasksByDate.splice(i, 0, task);
+            //         break;
+            //     } else if (newDeadline == iDeadline) {
+            //         if (i === numTasks - 1) {
+            //             userTasksByDate.splice(i + 1, 0, task);
+            //             break;
+            //         } else {
+            //             const nextDeadline = new Date(
+            //                 (
+            //                     await Task.findById(userTasksByDate[i + 1])
+            //                 ).dueDate
+            //             ).getTime();
+            //             if (newDeadline < nextDeadline) {
+            //                 userTasksByDate.splice(i + 1, 0, task);
+            //                 break;
+            //             }
+            //         }
+            //     } else if (i === numTasks - 1) {
+            //         userTasksByDate[numTasks] = task;
+            //     }
+            // }
             // Insert new Task into user's array of tasks that is sorted by priority
-            for (let i = 0; i < numTasks; i++) {
-                const iTask = await Task.findById(userTasksByPriority[i]);
-                if (task.priority === "High") {
-                    if (iTask.priority !== "High") {
-                        userTasksByPriority.splice(i, 0, task);
-                        break;
-                    } else {
-                        const iDeadline = new Date(iTask.dueDate).getTime();
-                        if (newDeadline < iDeadline) {
-                            userTasksByPriority.splice(i, 0, task);
-                            break;
-                        } else if (newDeadline == iDeadline) {
-                            if (i === numTasks - 1) {
-                                userTasksByPriority.splice(i + 1, 0, task);
-                                break;
-                            } else {
-                                const nextDeadline = new Date(
-                                    (
-                                        await Task.findById(
-                                            userTasksByPriority[i + 1]
-                                        )
-                                    ).dueDate
-                                ).getTime();
-                                if (newDeadline < nextDeadline) {
-                                    userTasksByPriority.splice(i + 1, 0, task);
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    if (iTask.priority === "Low") {
-                        const iDeadline = new Date(iTask.dueDate).getTime();
-                        if (newDeadline < iDeadline) {
-                            userTasksByPriority.splice(i, 0, task);
-                            break;
-                        } else if (newDeadline == iDeadline) {
-                            userTasksByPriority.splice(i + 1, 0, task);
-                            break;
-                        }
-                    }
-                }
-                if (i === numTasks - 1) {
-                    userTasksByPriority[numTasks] = task;
-                }
-            }
+            // for (let i = 0; i < numTasks; i++) {
+            //     const iTask = await Task.findById(userTasksByPriority[i]);
+            //     if (task.priority === "High") {
+            //         if (iTask.priority !== "High") {
+            //             userTasksByPriority.splice(i, 0, task);
+            //             break;
+            //         } else {
+            //             const iDeadline = new Date(iTask.dueDate).getTime();
+            //             if (newDeadline < iDeadline) {
+            //                 userTasksByPriority.splice(i, 0, task);
+            //                 break;
+            //             } else if (newDeadline == iDeadline) {
+            //                 if (i === numTasks - 1) {
+            //                     userTasksByPriority.splice(i + 1, 0, task);
+            //                     break;
+            //                 } else {
+            //                     const nextDeadline = new Date(
+            //                         (
+            //                             await Task.findById(
+            //                                 userTasksByPriority[i + 1]
+            //                             )
+            //                         ).dueDate
+            //                     ).getTime();
+            //                     if (newDeadline < nextDeadline) {
+            //                         userTasksByPriority.splice(i + 1, 0, task);
+            //                         break;
+            //                     }
+            //                 }
+            //             }
+            //         }
+            //     } else {
+            //         if (iTask.priority === "Low") {
+            //             const iDeadline = new Date(iTask.dueDate).getTime();
+            //             if (newDeadline < iDeadline) {
+            //                 userTasksByPriority.splice(i, 0, task);
+            //                 break;
+            //             } else if (newDeadline == iDeadline) {
+            //                 userTasksByPriority.splice(i + 1, 0, task);
+            //                 break;
+            //             }
+            //         }
+            //     }
+            //     if (i === numTasks - 1) {
+            //         userTasksByPriority[numTasks] = task;
+            //     }
+            // }
         }
 
-        courseTasks[courseTasks.length] = task;
+        courseTasksByDate[courseTasksByDate.length] = task;
+        console.log("reached here");
         await task.save();
-        await user.save();
+        // await user.save();
         await course.save();
         return res
             .status(201)
@@ -192,9 +285,9 @@ exports.getTasksByPriorityForUser = async (req, res) => {
 exports.getTasksForCourse = async (req, res) => {
     try {
         const course = await Course.findById(req.body.courseid).populate(
-            "tasks"
+            "tasksByDate"
         );
-        return res.json(course.tasks);
+        return res.json(course.tasksByDate);
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
