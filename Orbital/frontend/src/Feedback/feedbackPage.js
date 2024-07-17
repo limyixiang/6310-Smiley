191ca3639493d0987a3c196846ad8fbd51f47296
ZhengXinning <zhengxinning111@gmail.com>
2024-07-17 18:56:12 +0800

    css styling for settings and feedback pages
    styled elements for these two pages and ensure color customisation consistency

--- Orbital/frontend/src/Feedback/feedbackPage.js
+++ Orbital/frontend/src/Feedback/feedbackPage.js
@@ -1,7 +1,8 @@
-import { React, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { React, useState, useMemo, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./feedbackPage.module.css";
-import { submitFeedback } from "../Backend";
+import { submitFeedback, getColorTheme } from "../Backend";
+import logo from "../Login/smileytransparent.jpg";
 
 function FeedbackPage() {
     const location = useLocation();
@@ -16,9 +17,75 @@ function FeedbackPage() {
     const { feedbackType, feedbackTitle, feedbackMessage } = feedbackDetails;
 
     const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+
+    const themes = useMemo(
+        () => ({
+            light: {
+                "--mainText-color": "rgb(0, 0, 0)",
+                "--pageBackground-color": "rgb(255, 255, 255)",
+                "--elementBackground-color": "rgb(255, 255, 255)",
+                "--border-color": "rgba(153, 153, 153, 0.4)",
+                "--hover-color": "rgb(217, 217, 217)",
+                "--focus-color": "rgb(211, 211, 211)",
+            },
+            dark: {
+                "--mainText-color": "rgb(187, 187, 187)",
+                "--pageBackground-color": "rgb(0, 0, 0)",
+                "--elementBackground-color": "rgb(23, 23, 23)",
+                "--border-color": "rgba(212, 212, 212, 0.4)",
+                "--hover-color": "grey",
+                "--focus-color": "rgb(67, 67, 67)",
+            },
+            default: {
+                "--mainText-color": "rgb(0, 0, 66)",
+                "--pageBackground-color": "#d8f5fd",
+                "--elementBackground-color": "rgb(248, 254, 255)",
+                "--border-color": "rgba(71, 180, 230, 0.4)",
+                "--hover-color": "#c5edff",
+                "--focus-color": "rgba(118, 198, 235, 0.4)",
+            },
+            green: {
+                "--mainText-color": "rgb(0, 66, 17)",
+                "--pageBackground-color": "rgb(199, 228, 207)",
+                "--elementBackground-color": "rgb(241, 255, 245)",
+                "--border-color": "rgba(0, 160, 35, 0.4)",
+                "--hover-color": "rgb(210, 246, 219)",
+                "--focus-color": "rgba(0, 178, 118, 0.483)",
+            },
+            pink: {
+                "--mainText-color": "rgb(51, 0, 48)",
+                "--pageBackground-color": "#fff2fa",
+                "--elementBackground-color": "rgb(255, 254, 255)",
+                "--border-color": "rgba(255, 139, 249, 0.4)",
+                "--hover-color": "#fee1ff",
+                "--focus-color": "rgba(235, 149, 224, 0.4)",
+            },
+        }),
+        []
+    );
+
+    useEffect(() => {
+        const setThemeColor = (themeName) => {
+            const selectedTheme = themes[themeName];
+            for (const key in selectedTheme) {
+                document.documentElement.style.setProperty(
+                    key,
+                    selectedTheme[key]
+                );
+            }
+        };
+
+        getColorTheme({ userid: user._id })
+            .then((data) => {
+                setThemeColor(data.data);
+            })
+            .catch((err) => console.error("Error fetching data:", err));
+    }, [user._id, themes]);
 
     const handleChange = (e) => {
         setSuccess(false);
+        setError(false);
         const { name, value } = e.target;
         setFeedbackDetails((prevDetails) => ({
             ...prevDetails,
@@ -28,6 +95,16 @@ function FeedbackPage() {
 
     const onSubmit = (e) => {
         e.preventDefault();
+
+        if (
+            feedbackType === "" ||
+            feedbackTitle === "" ||
+            feedbackMessage === ""
+        ) {
+            setError("Please fill in all fields!");
+            return;
+        }
+
         submitFeedback(feedbackDetails).then((response) => {
             if (response) {
                 setSuccess(true);
@@ -40,23 +117,55 @@ function FeedbackPage() {
             feedbackMessage: "",
         }));
     };
+
+    const navigate = useNavigate();
+
+    const onLandingPage = () => {
+        navigate("/landingpage", { state: { user } });
+    };
+
     const successMessage = () => {
-        if (success) {
-            return (
-                <div>
+        return (
+            success && (
+                <div
+                    className={styles.successMessage}
+                    style={{ display: success ? "" : "none", color: "green" }}
+                >
                     Feedback submitted successfully! Thank you for your
                     feedback!
+                    <img
+                        src={logo}
+                        alt="SmileyLogo"
+                        style={{
+                            width: "5%",
+                            height: "5%",
+                            marginLeft: "10px",
+                        }}
+                    />
                 </div>
-            );
-        }
+            )
+        );
+    };
+
+    const errorMessage = () => {
+        return (
+            <div
+                className={styles.errorMessage}
+                style={{ display: error ? "" : "none", color: "red" }}
+            >
+                {error}
+            </div>
+        );
     };
 
     return (
-        <div className={styles.container}>
-            <h1>Leave us a feedback!</h1>
-            <form onSubmit={onSubmit}>
+        <div className={styles.mainContainer}>
+            <div className={styles.headerGroup}>
+                <h2>Leave us a Feedback!</h2>
+            </div>
+            <div className={styles.feedbackContainer}>
                 {/* Feedback Type Dropdown */}
-                <div>
+                <div className={styles.feedbackItem}>
                     <label htmlFor="feedbackType">Feedback Type:</label>
                     <select
                         name="feedbackType"
@@ -75,7 +184,7 @@ function FeedbackPage() {
                 </div>
 
                 {/* Feedback Title */}
-                <div>
+                <div className={styles.feedbackItem}>
                     <label htmlFor="feedbackTitle">Feedback Title:</label>
                     <input
                         type="text"
@@ -89,7 +198,7 @@ function FeedbackPage() {
                 </div>
 
                 {/* Feedback Message */}
-                <div>
+                <div className={styles.feedbackItem}>
                     <label htmlFor="feedbackMessage">Feedback Message:</label>
                     <textarea
                         name="feedbackMessage"
@@ -101,9 +210,14 @@ function FeedbackPage() {
                     ></textarea>
                 </div>
                 {successMessage()}
-
-                <button type="submit">Submit</button>
-            </form>
+                {errorMessage()}
+            </div>
+            <div className={styles.buttonGroup}>
+                <button onClick={onSubmit}>Submit</button>
+            </div>
+            <div className={styles.linkGroup}>
+                <button onClick={onLandingPage}>Back to Landing Page</button>
+            </div>
         </div>
     );
 }
--- Orbital/frontend/src/Settings/settingsPage.js
+++ Orbital/frontend/src/Settings/settingsPage.js
@@ -1,8 +1,8 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect, useMemo } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./settingsPage.module.css";
 import ToggleSwitch from "./toggleSwitch";
-import { updateNotifications, getPreferences } from "../Backend";
+import { updateNotifications, getPreferences, getColorTheme } from "../Backend";
 
 function SettingsPage() {
     const location = useLocation();
@@ -14,6 +14,52 @@ function SettingsPage() {
 
     const [success, setSuccess] = useState(false);
     const [loading, setLoading] = useState(false);
+
+    const themes = useMemo(
+        () => ({
+            light: {
+                "--mainText-color": "rgb(0, 0, 0)",
+                "--pageBackground-color": "rgb(255, 255, 255)",
+                "--elementBackground-color": "rgb(255, 255, 255)",
+                "--border-color": "rgba(153, 153, 153, 0.4)",
+                "--hover-color": "rgb(217, 217, 217)",
+                "--focus-color": "rgb(211, 211, 211)",
+            },
+            dark: {
+                "--mainText-color": "rgb(187, 187, 187)",
+                "--pageBackground-color": "rgb(0, 0, 0)",
+                "--elementBackground-color": "rgb(23, 23, 23)",
+                "--border-color": "rgba(212, 212, 212, 0.4)",
+                "--hover-color": "grey",
+                "--focus-color": "rgb(67, 67, 67)",
+            },
+            default: {
+                "--mainText-color": "rgb(0, 0, 66)",
+                "--pageBackground-color": "#d8f5fd",
+                "--elementBackground-color": "rgb(248, 254, 255)",
+                "--border-color": "rgba(71, 180, 230, 0.4)",
+                "--hover-color": "#c5edff",
+                "--focus-color": "rgba(118, 198, 235, 0.4)",
+            },
+            green: {
+                "--mainText-color": "rgb(0, 66, 17)",
+                "--pageBackground-color": "rgb(199, 228, 207)",
+                "--elementBackground-color": "rgb(241, 255, 245)",
+                "--border-color": "rgba(0, 160, 35, 0.4)",
+                "--hover-color": "rgb(210, 246, 219)",
+                "--focus-color": "rgba(0, 178, 118, 0.483)",
+            },
+            pink: {
+                "--mainText-color": "rgb(51, 0, 48)",
+                "--pageBackground-color": "#fff2fa",
+                "--elementBackground-color": "rgb(255, 254, 255)",
+                "--border-color": "rgba(255, 139, 249, 0.4)",
+                "--hover-color": "#fee1ff",
+                "--focus-color": "rgba(235, 149, 224, 0.4)",
+            },
+        }),
+        []
+    );
 
     useEffect(() => {
         getPreferences({ userid: user._id }).then((response) => {
@@ -32,7 +78,23 @@ function SettingsPage() {
                 });
             }
         });
-    }, [user._id]);
+
+        const setThemeColor = (themeName) => {
+            const selectedTheme = themes[themeName];
+            for (const key in selectedTheme) {
+                document.documentElement.style.setProperty(
+                    key,
+                    selectedTheme[key]
+                );
+            }
+        };
+
+        getColorTheme({ userid: user._id })
+            .then((data) => {
+                setThemeColor(data.data);
+            })
+            .catch((err) => console.error("Error fetching data:", err));
+    }, [user._id, themes]);
 
     const {
         notifications,
@@ -134,9 +196,16 @@ function SettingsPage() {
     };
 
     const successMessage = () => {
-        if (success) {
-            return <h4>Settings saved successfully!</h4>;
-        }
+        return (
+            success && (
+                <div
+                    className={styles.successMessage}
+                    style={{ display: success ? "" : "none", color: "green" }}
+                >
+                    Settings saved successfully!
+                </div>
+            )
+        );
     };
 
     const loadingMessage = () => {
@@ -155,7 +224,7 @@ function SettingsPage() {
             <div className={styles.headerGroup}>
                 <h2>Settings Page</h2>
             </div>
-            <div>
+            <div className={styles.settingsContainer}>
                 <div className={styles.settingItem}>
                     <label>Allow Push Notifications</label>
                     <ToggleSwitch
@@ -249,12 +318,14 @@ function SettingsPage() {
                 {successMessage()}
                 <div className={styles.buttonGroup}>
                     <button onClick={onSave}>Save</button>
-                    <button onClick={onReset}>Reset to default</button>
+                    <button onClick={onReset}>Reset to Default</button>
+                </div>
+                <div className={styles.linkGroup}>
                     <button onClick={onLandingPage}>
                         Back to Landing Page
                     </button>
                     <button onClick={onFeedbackPage}>
-                        Leave us a feedback!
+                        Leave us a Feedback!
                     </button>
                 </div>
             </div>
